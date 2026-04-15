64931e26fc4c647385b0e5db037479f72b7d7f00
Sureddynithinreddy <nithinsureddy2005@gmail.com>
2026-04-15 09:30:15 +0530

    fix: cors options path for Express 6

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); /// ← handle ALL preflight requests
+app.options(/.*/, cors(corsOptions));/// ← handle ALL preflight requests
 
 /*
 ---------------------------------------
